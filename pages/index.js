c2bc42b17c7f86ad6239e22c1a650db0fbbbb5be
LizethVictoria20 <lizvictoria754@gmail.com>
2021-11-29 11:41:18 +0100

    Home with main picture

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,39 @@
+import styles from '../styles/Home/Home.module.css';
+import Image from 'next/image';
+import Link from 'next/link'
 
-import Home from './Home'
-export default function Index() {
+export const getServerSideProps = async () => {
+  console.log(process.env.API_KEY);
+
+  const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`);
+  const data = await res.json()
+  return {
+    props: {
+      pictures: data
+    }
+  }
+}
+
+
+export default function Home({ pictures }) {
   return (
     <div>
-      <Home />
+      {/* { pictures.map(picture => (
+        <div key={picture.title}>
+          <Image 
+            src={picture.url}
+            alt='Holis'
+            width='400'
+            height='400'
+          />
+        </div>
+      )) } */}
+      <Image 
+          src={pictures.url}
+          alt='Holis'
+          width='400'
+          height='400'
+      />
     </div>
   )
 }
