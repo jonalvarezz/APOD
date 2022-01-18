5d112bfd5179b298f5fb87eeede3c488758f02ac
LizethVictoria20 <lizvictoria754@gmail.com>
2022-01-18 11:11:17 -0500

    query params

---
+++ pages/date.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function date() {
+  return (
+    <div>
+      <a href={pictures.url}>h</a>
+    </div>
+  )
+}
+
+export default date
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,14 @@ import Image from 'next/image';
 import Link from 'next/link'
 
 export const getServerSideProps = async () => {
-  console.log(process.env.API_KEY);
 
   const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`);
+  const statusCode = res.status > 200 ? res.status : false;
   const data = await res.json()
   return {
     props: {
-      pictures: data
+      pictures: data,
+      statusCode
     }
   }
 }
@@ -18,22 +19,14 @@ export const getServerSideProps = async () => {
 export default function Home({ pictures }) {
   return (
     <div>
-      {/* { pictures.map(picture => (
-        <div key={picture.title}>
-          <Image 
-            src={picture.url}
+      <a href={`date?${pictures.date}`}>
+        <Image 
+            src={pictures.url}
             alt='Holis'
             width='400'
             height='400'
-          />
-        </div>
-      )) } */}
-      <Image 
-          src={pictures.url}
-          alt='Holis'
-          width='400'
-          height='400'
-      />
+        />
+      </a> 
     </div>
   )
 }
